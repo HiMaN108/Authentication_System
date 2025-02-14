a07a421ac7d5c6716827caaf080d717b85b855cc
Himanshu Maurya <him2000.maurya@gmail.com>
2025-02-14 16:19:09 +0530

    updating the some files

--- server/database/conn.js
+++ server/database/conn.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+<<<<<<< HEAD
 import { MongoMemoryServer } from "mongodb-memory-server";
 
 import dotenv from 'dotenv';
@@ -15,4 +16,26 @@ export default async function connect(){
     const db = await mongoose.connect(process.env.ATLAS_URI);
     console.log('Database Connected');
     return db;  
+=======
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default async function connect() {
+  try {
+    mongoose.set("strictQuery", true);
+
+    // Use MongoDB Atlas URI from .env file
+    const db = await mongoose.connect(process.env.ATLAS_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("Database Connected");
+    return db;
+  } catch (error) {
+    console.error("Database connection error:", error.message);
+    process.exit(1); // Exit the process with an error code
+  }
+>>>>>>> fea394d (updating the some files)
 }
